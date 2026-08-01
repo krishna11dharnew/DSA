05b9892adf2b3bd746dafb6ee220710631e8f19e
KRISHNA DHARMENDRA <2300520100040@ietlucknow.ac.in>
2026-08-01 14:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (89.24%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,9 +1,34 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size()!=goal.size()) return false;
-        string temp=s+s;
-        return temp.find(goal)!=string::npos;
+        // if(s.size()!=goal.size()) return false;
+        // string temp=s+s;
+        // return temp.find(goal)!=string::npos;
+
+
+
+
+        if(s.size()!=goal.size())
+        {
+            return false;
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            if(s==goal) return true;
+            char start=s[0];
+            s.erase(s.begin());
+            s.push_back(start);
+        }
+        return false;
         
     }
 };
+
+
+// string::npos kya hai?
+
+// Ye ek special constant hai jo batata hai:
+
+// "String nahi mili."
+
+// Jab find() search karta hai aur substring nahi milti, to wo string::npos return karta hai.
