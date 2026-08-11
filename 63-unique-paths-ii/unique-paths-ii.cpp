0177d4fdcacdd41c8c22f715bb7bf5e84a68fdea
KRISHNA DHARMENDRA <2300520100040@ietlucknow.ac.in>
2026-08-11 14:57:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (73.01%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n=obstacleGrid.size();
+        int m=obstacleGrid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        dp[0][0]=1;
+        for(int j=1;j<m;j++)
+        {
+            if(obstacleGrid[0][j]==1)
+            {
+                dp[0][j]=0;
+                continue;
+            }
+            dp[0][j]=dp[0][j-1];
+        }
+        for(int i=1;i<n;i++)
+        {
+            
+            if(obstacleGrid[i][0]==1)
+            {
+                dp[i][0]=0;
+                continue;
+            }
+            dp[i][0]=dp[i-1][0];
+        }
+        if(obstacleGrid[0][0]==1)return 0 ;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(obstacleGrid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
