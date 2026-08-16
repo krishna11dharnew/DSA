8b08d53d53cbb71c1edf063fb26f14adf26fb4e9
KRISHNA DHARMENDRA <2300520100040@ietlucknow.ac.in>
2026-08-16 17:50:43 +0530

    Time: 107 ms (8.86%) | Memory: 104.6 MB (6.37%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums)  {
-        int i=0;
-        int n=nums.size();
-        while(i<n)
-        {
-            int correct=nums[i]-1;
-            if(nums[i]!=nums[correct])
-            {
-                swap(nums[i],nums[correct]);
-            }
-            else
-            {
-                i++;
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=i+1)return nums[i];
-        }
-        return n;
+    //     int i=0;
+    //     int n=nums.size();
+    //     while(i<n)
+    //     {
+    //         int correct=nums[i]-1;
+    //         if(nums[i]!=nums[correct])
+    //         {
+    //             swap(nums[i],nums[correct]);
+    //         }
+    //         else
+    //         {
+    //             i++;
+    //         }
+    //     }
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(nums[i]!=i+1)return nums[i];
+    //     }
+    //     return n;
+    // }
+    unordered_map<int,int>mp;
+    for(int i=0;i<nums.size();i++)
+    {
+        mp[nums[i]]++;
+
+    }
+    for(auto it:mp)
+    {
+        if(it.second>1)return it.first;
+
+    }
+    return -1;
     }
 };
