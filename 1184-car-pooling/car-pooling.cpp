58a91ae49ea3fe1dd7f2289ddb57a349218daada
KRISHNA DHARMENDRA <2300520100040@ietlucknow.ac.in>
2026-09-04 00:29:41 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (67.00%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -27,22 +27,55 @@ public:
     //     return true;
     // }
 
-    vector<int>diff(1001,0);
-    for(auto it:trips)
-    {
-        int pass=it[0];
-        int from=it[1];
-        int to=it[2];
-        diff[from]+=pass;
-        diff[to]-=pass;
-    }
-    int currpass=0;
-    for(int i=0;i<=1000;i++)
-    {
-         currpass+=diff[i];
-         if(currpass>capacity) return false;
+    // vector<int>diff(1001,0);
+    // for(auto it:trips)
+    // {
+    //     int pass=it[0];
+    //     int from=it[1];
+    //     int to=it[2];
+    //     diff[from]+=pass;
+    //     diff[to]-=pass;
+    // }
+    // int currpass=0;
+    // for(int i=0;i<=1000;i++)
+    // {
+    //      currpass+=diff[i];
+    //      if(currpass>capacity) return false;
 
-    }
-    return true;
+    // }
+    // return true;
+
+    
+        vector<pair<int,int>> start, end;
+
+        for (auto &trip : trips) {
+            start.push_back({trip[1], trip[0]}); // {pickup, passengers}
+            end.push_back({trip[2], trip[0]});   // {drop, passengers}
+        }
+
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+
+        int i = 0, j = 0;
+        int curr = 0;
+         
+        while (i < start.size()&&j<end.size()) {
+
+            if (start[i].first < end[j].first) {
+                curr += start[i].second;
+
+                if (curr > capacity)
+                    return false;
+
+                i++;
+            }
+            else {
+                curr -= end[j].second;
+                j++;
+            }
+        }
+
+        return true;
+    
  }
 };
